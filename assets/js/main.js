f9bb5eabc41c9ce1fdbcca8cd3a2950fc78bf14d
hasankhan007 <hasanmrj6458@gmail.com>
2021-07-01 19:22:04 +0600

    xenon-last-1721

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
+    
+    /* image magnific popup */
     $('.project-popup').magnificPopup({
         type: 'image',
         gallery: {
             enabled: true
         }
     });
+
+/* counterup activation */
+    $('.counter').counterUp();
+
+
+
+
 });
+
+
+
