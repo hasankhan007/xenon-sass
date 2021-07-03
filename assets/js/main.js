68b407fff7d30ff2f25f9836d56ac695c4ceaebe
hasankhan007 <hasanmrj6458@gmail.com>
2021-07-03 19:42:44 +0600

    last-xenon

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,15 @@
      /* counterup activation */
      $('.counter').counterUp();
 
+     /* slick slider */
+     $('.testimonials').slick({
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots:true,
+     });
+
 
 
 
