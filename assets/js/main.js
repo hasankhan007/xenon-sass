161cbfef25fb7fa31bbdd9fd0709fcc91187a1c8
hasankhan007 <hasanmrj6458@gmail.com>
2021-07-06 01:17:09 +0600

    lase-xenon

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,46 @@
         dots:true,
      });
 
+     /* Top to back */
+     $(".top").on('click', function () {
+         $("body, html").animate({
+             scrollTop: 0
+         }, 1000);
+         return false;
+     });
+
+     /* Top to backbtn Auto hide */
+     $(window).on('scroll', function () {
+         if ($(this).scrollTop() > 40) {
+             $('.top').fadeIn(1000);
+         } else {
+             $('.top').fadeOut(1000);
+         }
+     });
+
+     /* Social Button Auto hide */
+     $(window).on('scroll', function () {
+         if ($(this).scrollTop() > 40) {
+             $('.socialBtn').fadeIn(500);
+         } else {
+             $('.socialBtn').fadeOut(500);
+         }
+     });
+
+
+     /* TOP Menu Stick  */
+     $(window).on('scroll', function () {
+         if ($(this).scrollTop() > 100) {
+             $('#sticky-header').addClass("sticky");
+         } else {
+             $('#sticky-header').removeClass("sticky");
+         }
+     });
+
+
+     /* One page scroll smooth  */
+     $('#nav').onePageNav();
+
 
 
 
